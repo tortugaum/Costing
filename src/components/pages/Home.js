c0495d9de85baee0b15b32d1fa367898fa3c357d
tortugaum <luizfelipeforcat@hotmail.com>
2023-01-06 00:24:34 -0300

    Pagina home criada. LinkButton criado

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,17 @@
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg';
+import LinkButton from '../layout/LinkButton';
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem vindo ao <span>Costing</span>
+      </h1>
+      <p>Gerencie os seus projetos agora!</p>
+      <LinkButton to='/NewProject' text='Criar Projeto' />
+      <img src={savings} alt='costings' />
+    </section>
+  );
 }
 
 export default Home;
